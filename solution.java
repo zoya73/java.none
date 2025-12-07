6888e2a9bbbdb247ce3da0e829f3e254978f3dd2
zoya73 <zoyarukshar73@gmail.com>
2025-12-07 16:56:59 +0000

    soultion

---
+++ solution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class solution{
+    public static void main (String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        System.out.println("the num is enter the by the user is :" +num);
+    }
+}
